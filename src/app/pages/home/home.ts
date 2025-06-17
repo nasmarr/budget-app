012895dc495cb5013097082d607443fa73fa7bb5
nmarmelo <nathanmarmelo@hotmail.com>
2025-06-17 16:18:23 -0400

    added transaction type toggle to home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CurrencyInput } from '../../components/currency-input/currency-input';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-home',
-  imports: [CurrencyInput],
+  imports: [CurrencyInput, MatButtonToggleModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
