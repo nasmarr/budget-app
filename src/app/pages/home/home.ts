d599d3667fe79a29f2fe7eb4a485e7040c3d92f6
nmarmelo <nathanmarmelo@hotmail.com>
2025-06-17 16:46:19 -0400

    added datepicker to home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CurrencyInput } from '../../components/currency-input/currency-input';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
 
 @Component({
   selector: 'app-home',
-  imports: [CurrencyInput, MatButtonToggleModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    CurrencyInput,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
+
 export class Home {
 
 }
