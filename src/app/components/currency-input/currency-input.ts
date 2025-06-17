f541afa1310aaf2ed3180c174db4a77c25a33651
nmarmelo <nathanmarmelo@hotmail.com>
2025-06-17 15:36:44 -0400

    applied simple formatting to currency input

--- src/app/components/currency-input/currency-input.ts
+++ src/app/components/currency-input/currency-input.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatInput } from '@angular/material/input';
+import { MatFormField } from '@angular/material/input';
 import { NgxCurrencyDirective} from 'ngx-currency';
 
 @Component({
   selector: 'app-currency-input',
-  imports: [NgxCurrencyDirective],
+  imports: [NgxCurrencyDirective, MatInput, MatFormField],
   templateUrl: './currency-input.html',
   styleUrl: './currency-input.scss'
 })
