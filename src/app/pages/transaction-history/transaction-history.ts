9d3c996221c8ead1affd52567cc261b99d84b6c4
nasmarr <nasmarr@proton.me>
2025-12-28 22:04:47 -0500

    Add basic filters to Transaction History page

--- src/app/pages/transaction-history/transaction-history.ts
+++ src/app/pages/transaction-history/transaction-history.ts
@@ -3,21 +3,49 @@ import { Observable } from 'rxjs';
 import { AsyncPipe, DatePipe, CurrencyPipe } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 import { Transaction } from '../../models';
 import { TransactionService } from '../../services';
 
+type FilterPeriod = 'all' | 'month' | 'year';
+
 @Component({
   selector: 'app-transaction-history',
-  imports: [AsyncPipe, DatePipe, CurrencyPipe, MatCardModule, MatIconModule],
+  imports: [AsyncPipe, DatePipe, CurrencyPipe, MatCardModule, MatIconModule, MatChipsModule],
   templateUrl: './transaction-history.html',
   styleUrl: './transaction-history.scss',
 })
 export class TransactionHistory implements OnInit {
   transactions$!: Observable<Transaction[]>;
+  selectedFilter: FilterPeriod = 'all';
 
   constructor(private transactionService: TransactionService) {}
 
   ngOnInit(): void {
-    this.transactions$ = this.transactionService.getTransactions();
+    this.loadTransactions();
+  }
+
+  setFilter(filter: FilterPeriod): void {
+    this.selectedFilter = filter;
+    this.loadTransactions();
+  }
+
+  private loadTransactions(): void {
+    const now = new Date();
+    const currentMonth = now.getMonth();
+    const currentYear = now.getFullYear();
+
+    switch (this.selectedFilter) {
+      case 'month':
+        this.transactions$ = this.transactionService.getTransactionsByMonth(currentMonth, currentYear);
+        break;
+      case 'year':
+        this.transactions$ = this.transactionService.getTransactionsByYear(currentYear);
+        break;
+      case 'all':
+      default:
+        this.transactions$ = this.transactionService.getTransactions();
+        break;
+    }
   }
 }
